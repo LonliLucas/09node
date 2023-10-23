5ce728f70f60e4bd3cb9c33d89c44acc94c4796d
LonliLucas <lukej.gilless@gmail.com>
2023-10-23 00:15:13 -0600

    Added questions and installed inquirer.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,49 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'Title',
+},
+{
+    type: 'input',
+    message: 'Give a description of your project:',
+    name: 'Description'
+},
+{
+    type: 'input',
+    message: 'Enter installation instructions:',
+    name: 'Installation'
+},
+{
+    type: 'input',
+    message: 'Describe the usage information:',
+    name: 'Usage'
+},
+{
+    type: 'input',
+    message: 'Who else contributed to the project?',
+    name: 'Contributing'
+},
+{
+    type: 'input',
+    message: 'Give instructions for testing your project:',
+    name: 'Tests'
+},
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
